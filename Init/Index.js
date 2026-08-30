7ecde2de842c98cf4b54e84de32e67591ccb9c38
Surajdeveloper123 <sbind4057@gmail.com>
2026-08-30 17:13:47 +0530

    fix: update listing categories in data

--- Init/Index.js
+++ Init/Index.js
@@ -1,26 +1,27 @@
-const mongoose =require("mongoose");
-const initData =require("./Data.js");
-const Listing =require("../models/Listing.js");
-
- const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 
-main()
-.then(() =>{
-    console.log("connect to DB");
+ const mongoose =require("mongoose");
+ const initData =require("./Data.js");
+ const Listing =require("../models/Listing.js");
 
-})
-.catch((err) =>{
-    console.log(err);
-});
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+ main()
+ .then(() =>{
+     console.log("connect to DB");
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+ })
+ .catch((err) =>{
+     console.log(err);
+ });
+
+ async function main(){
+     await mongoose.connect(MONGO_URL);
 }
-    const initDB =async () =>{
+     const initDB =async () =>{
         await Listing.deleteMany({});
         initData.data=initData.data.map((obj)=>({...obj,owner: "6a7eb736d04e12a87e8e8ce6"}));
         await Listing.insertMany(initData.data);
         console.log("data was initialization");
-    };
+     };
     initDB();
+    
