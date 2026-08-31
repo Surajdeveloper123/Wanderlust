238b605bf3f703b395a39327532b2f72285d0797
Surajdeveloper123 <sbind4057@gmail.com>
2026-09-01 00:45:49 +0530

    Fix nested listing bookings route

--- routes/booking.js
+++ routes/booking.js
@@ -4,7 +4,6 @@ const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn } = require("../views/middleware");
 const bookingController = require("../controllers/bookings");
 
-router.post("/", isLoggedIn, wrapAsync(bookingController.createBooking));
 router.get("/", isLoggedIn, wrapAsync(async (req, res) => {
     let allBookings = await Booking.find({}).populate("listing");
     res.render("bookings/index.ejs", { allBookings });
